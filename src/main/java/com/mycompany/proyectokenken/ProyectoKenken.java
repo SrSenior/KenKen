76e7b5d0ecd3abf4ea4bd22ba59908672f629830
Kava52 <kennethandrey5252@gmail.com>
2023-11-08 15:27:04 -0600

    Nuevo archivo

---
+++ src/main/java/com/mycompany/proyectokenken/ProyectoKenken.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectokenken;
+
+/**
+ *
+ * @author kenne
+ */
+public class ProyectoKenken {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
