abd9ea6846dec42cbc5e8991a406d933a857ab0b
Kava52 <kennethandrey5252@gmail.com>
2023-11-08 22:39:22 -0600

    Todo documentado de momento

---
+++ src/main/java/Informacion/Ayuda.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Informacion;
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ *Clase que despliega el manual de usuario
+ * @author Jose Pablo Vega Solano
+ */
+public class Ayuda {
+    
+    public static void abrirManual(){
+        try {
+            String ruta= "Ruta";
+            File file = new File(ruta);
+            if (file.exists()){
+                if (Desktop.isDesktopSupported()){
+                    Desktop.getDesktop().open(file);
+                }
+            }
+        } catch (IOException e) {
+        }
+    }
+    
+}
